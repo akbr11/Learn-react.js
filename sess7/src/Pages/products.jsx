4e644c777db1e33f75b781d0acc42d07741d3cd8
akbr11 <ciremaiindah3078@gmail.com>
2023-10-06 12:51:17 +0700

    rendering list

--- sess7/src/Pages/products.jsx
+++ sess7/src/Pages/products.jsx
@@ -1,18 +1,37 @@
 import CardProducts from "../component/Fragments/CardProducts";
 
+const products = [
+  {
+    id: 1,
+    name: "Sepatu Baru",
+    price: "Rp. 1.000.000",
+    image: "/images/shoes.jpg",
+    description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis sequi
+        asperiores quaerat eaque omnis harum repudiandae voluptatem ea officia
+        totam natus corporis, magnam ullam delectus sed laudantium vitae
+        mollitia rerum!`,
+  },
+  {
+    id: 2,
+    name: "Sepatu Lama",
+    price: "Rp. 4.000.000",
+    image: "/images/shoes.jpg",
+    description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. `,
+  },
+];
+
 const ProductPage = () => {
   return (
     <div className="flex justify-center py-5">
-      <CardProducts>
-        <CardProducts.Header image="/images/shoes.jpg" />
-        <CardProducts.Body title="Sepatu Baru">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis sequi
-          asperiores quaerat eaque omnis harum repudiandae voluptatem ea officia
-          totam natus corporis, magnam ullam delectus sed laudantium vitae
-          mollitia rerum!
-        </CardProducts.Body>
-        <CardProducts.Footer price="Rp. 1.000.000" />
-      </CardProducts>
+      {products.map((product) => (
+        <CardProducts>
+          <CardProducts.Header image={product.image} />
+          <CardProducts.Body name={product.name}>
+            {product.description}
+          </CardProducts.Body>
+          <CardProducts.Footer price={product.price} />
+        </CardProducts>
+      ))}
     </div>
   );
 };
