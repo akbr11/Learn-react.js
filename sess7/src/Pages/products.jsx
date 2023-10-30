b0a7a693e6369c9dc08805b543260a6774a013d3
akbr11 <ciremaiindah3078@gmail.com>
2023-10-30 21:08:08 +0700

    useEffect

--- sess7/src/Pages/products.jsx
+++ sess7/src/Pages/products.jsx
@@ -29,15 +29,18 @@ const ProductPage = () => {
   const [cart, setCart] = useState([]);
   const [totalPrice, setTotalPrice] = useState(0);
   useEffect(() => {
-    setCart([{ id: 1, qty: 1 }]);
+    setCart(JSON.parse(localStorage.getItem("cart")) || []);
   }, []);
 
   useEffect(() => {
-    const sum = cart.reduce((acc, item) => {
-      const product = products.find((product) => product.id === item.id);
-      return acc + product.price * item.qty;
-    }, 0);
-    setTotalPrice(sum);
+    if (cart.length > 0) {
+      const sum = cart.reduce((acc, item) => {
+        const product = products.find((product) => product.id === item.id);
+        return acc + product.price * item.qty;
+      }, 0);
+      setTotalPrice(sum);
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
   }, [cart]);
 
   const handlerLogout = () => {
